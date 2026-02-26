5d221ee044025f18151c6cf957f108a387fa260a
MD SHARIFUL ISLAM <94898754+msharifulvisionary@users.noreply.github.com>
2026-02-26 13:12:31 +0600

    Update types.ts

--- types.ts
+++ types.ts
@@ -109,7 +109,7 @@ export interface Manager {
   iftaarConfig?: IftaarConfig;
   extraRice?: ExtraRice[]; // New: Extra rice usage
   globalAdditionalRicePots?: number; // New: Global Additional Rice Expense
-  fixedMealCount?: number; // New: Fixed meal system
+  fixedMeal?: number; // New: Fixed Meal System
 }
 
 export interface IftaarConfig {
